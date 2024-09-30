8a287d680ccc04e11ce2e5c0297e710857e25249
Duc Duong <112845152+spartan-ductduong@users.noreply.github.com>
2024-09-30 16:07:42 +0700

    Modify artifactId to core (#58)

--- buildSrc/src/main/kotlin/com/c0x12c/featureflag/utils/VersionUtil.kt
+++ buildSrc/src/main/kotlin/com/c0x12c/featureflag/utils/VersionUtil.kt
@@ -1,17 +1,17 @@
 package com.c0x12c.featureflag.utils
 
+import com.c0x12c.featureflag.model.Manifest
 import java.io.File
 import java.io.FileNotFoundException
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
 
 class VersionUtil {
   companion object {
     fun getVersionFromManifest(manifestFile: File): String {
       if (manifestFile.exists()) {
         val jsonString = manifestFile.readText()
-        val manifest = Json.decodeFromString<JsonObject>(jsonString)
-        return manifest.getValue("version").toString()
+        val manifest = Json.decodeFromString<Manifest>(jsonString)
+        return manifest.version
       } else {
         throw FileNotFoundException("Manifest file not found")
       }
